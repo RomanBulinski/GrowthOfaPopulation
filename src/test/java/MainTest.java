59fcc3a7549972923f54e86861b826bdb117c3e6
Roman Bulinski <aboandrus@gmail.com>
2019-04-24 18:53:14 +0200

    Added second test

--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -6,8 +6,13 @@ public class MainTest {
 
 
     @Test
-    public void test1(){
-        assertEquals( 15  ,Main.nbYear(1500, 5, 100, 5000)     );
+    public void test1() {
+        assertEquals(15, Main.nbYear(1500, 5, 100, 5000));
+    }
+
+    @Test
+    public void test2() {
+        assertEquals(10, Main.nbYear(1500000, 2.5, 10000, 2000000));
     }
 
 
